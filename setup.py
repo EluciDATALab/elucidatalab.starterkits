4bbfe51efc576e49a9fa6de7ef870c19b012f38d
henriquecabral-sirris <henrique.cabral@sirris.be>
2022-11-23 08:51:22 +0100

    moved setup.py file to root directory

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+	name="starterkits",
+	packages=find_packages(),
+	version="1.0.1",
+	description="A library of data science, ML and AI algorithms and code utilities",
+	author="EluciDATA Lab by Sirris",
+	author_email="t-dat@sirris.be",
+	url="https://github.com/EluciDATALab/elucidatalab.starterkits.git"
+)
