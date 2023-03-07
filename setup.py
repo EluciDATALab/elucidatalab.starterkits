66c885438c5f9f74685a0fc656bd97d353d3cb36
henriquecabral-sirris <henrique.cabral@sirris.be>
2023-03-07 13:36:22 +0100

    added requirements file to remove install_requires from setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,6 @@ setup(
 	packages=find_packages(),
 	version="1.0.1",
 	description="A library of data science, ML and AI algorithms and code utilities",
-	install_requires=['calmap'],
 	author="EluciDATA Lab by Sirris",
 	author_email="t-dat@sirris.be",
 	url="https://github.com/EluciDATALab/elucidatalab.starterkits.git"
